f6177f62a62b80b0c38a9bbe98268406ba14bd45
Andreas Otten <aotten77@googlemail.com>
2016-06-22 22:39:53 +0200

    no message

--- app/login/login.js
+++ app/login/login.js
@@ -18,7 +18,7 @@ angular.module('myApp.login', ['firebase.utils', 'firebase.auth', 'ngRoute'])
             console.log(authObject);
             var ref = fbutil.ref('users', authObject.uid);
             return fbutil.handler(function(cb) {
-              ref.set({email: authObject.google.email, name: authObject.google.cachedUserProfile.name, online: true}, cb);
+              ref.set({email: authObject.google.email, name: authObject.google.cachedUserProfile.name}, cb);
             });
           })
           .then(function(/* user */) {
